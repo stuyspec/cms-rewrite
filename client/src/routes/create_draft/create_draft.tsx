2c385be0be7fbafd7229bec61593cf4d78980db8
dchen278 <dchen30@stuy.edu>
2021-12-28 14:32:41 -0500

    add wysiwg editor

--- client/src/routes/create_draft/create_draft.tsx
+++ client/src/routes/create_draft/create_draft.tsx
@@ -4,136 +4,166 @@ import { useEffect, useState } from "react";
 import Draft from "../../types/Draft";
 import store from "../../store";
 import upload_image_helper from "../../helpers/upload_image";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { EditorState, convertToRaw } from "draft-js";
+import draftToHtml from 'draftjs-to-html';
 
 interface CreateDraftResponse {
-	draft: Draft;
-	description: string;
+  draft: Draft;
+  description: string;
 }
 
 function Create_Draft() {
-	const [coverImageURL, setCoverImageURL] = useState<string | null>(null);
+  const [value, setValue] = useState("");
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
 
-	const new_draft_handler = async () => {
-		console.log("Create new draft");
-		const volume: string = (document.getElementById("new_volume") as any)
-			.value;
-		const issue: string = (document.getElementById("new_issue") as any)
-			.value;
-		const title: string = (document.getElementById("new_title") as any)
-			.value;
-		const contributors: string[] = (
-			(document.getElementById("new_title") as any).value as string
-		).split(", ");
-		const cover_image_contributor: string = (
-			document.getElementById("new_cover_image_contributor") as any
-		).value;
-		const text: string = (document.getElementById("new_text") as any).value;
-		const summary: string = (document.getElementById("new_summary") as any)
-			.value;
-		const section_id: number = (
-			document.getElementById("new_section") as any
-		).value;
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+  const [coverImageURL, setCoverImageURL] = useState<string | null>(null);
 
-		const body = {
-			title: title,
-			contributors: contributors,
-			text: text,
-			volume: volume,
-			issue: issue,
-			section_id: section_id,
-			summary: summary,
-			cover_image: coverImageURL,
-			cover_image_contributor: cover_image_contributor,
-		};
-		if (coverImageURL) {
-			console.log({ body });
-			const r = await fetch("http://127.0.0.1:5678/api/db/create_draft", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					"auth-token": store.getState().validauthtoken.value,
-				},
-				body: JSON.stringify(body),
-			});
+  const new_draft_handler = async () => {
+	console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())))
+    console.log("Create new draft");
+    const volume: string = (document.getElementById("new_volume") as any).value;
+    const issue: string = (document.getElementById("new_issue") as any).value;
+    const title: string = (document.getElementById("new_title") as any).value;
+    const contributors: string[] = (
+      (document.getElementById("new_title") as any).value as string
+    ).split(", ");
+    const cover_image_contributor: string = (
+      document.getElementById("new_cover_image_contributor") as any
+    ).value;
+    const text: string = draftToHtml(convertToRaw(editorState.getCurrentContent()))
+    const summary: string = (document.getElementById("new_summary") as any)
+      .value;
+    const section_id: number = (document.getElementById("new_section") as any)
+      .value;
 
-			const rjson = (await r.json()) as CreateDraftResponse;
-			console.log(rjson);
-			window.location.replace("/draft/" + rjson.draft._id);
-		}
-	};
+    const body = {
+      title: title,
+      contributors: contributors,
+      text: text,
+      volume: volume,
+      issue: issue,
+      section_id: section_id,
+      summary: summary,
+      cover_image: coverImageURL,
+      cover_image_contributor: cover_image_contributor,
+    };
+    if (coverImageURL) {
+      console.log({ body });
+      const r = await fetch("http://127.0.0.1:5678/api/db/create_draft", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": store.getState().validauthtoken.value,
+        },
+        body: JSON.stringify(body),
+      });
 
-	const upload_cover_image = async () => {
-		// In case event listener remains, or is triggered manually, etc
-		const uploaded_files = (
-			document.getElementById("upload_cover_image") as any
-		).files;
-		if (uploaded_files.length > 0) {
-			const uploaded_file = uploaded_files[0]; // always grab first
-			const public_url = (await upload_image_helper(
-				uploaded_file
-			)) as string;
-			setCoverImageURL(public_url);
-		}
-	};
+      const rjson = (await r.json()) as CreateDraftResponse;
+      console.log(rjson);
+      window.location.replace("/draft/" + rjson.draft._id);
+    }
+  };
 
-	return (
-		<div>
-			<h1>Create Draft</h1>
-			<div id="new_draft_fom">
-				<h2>
-					Volume:&nbsp;
-					<input type="number" id="new_volume" />
-					&nbsp;|&nbsp; Issue:&nbsp;
-					<input type="number" id="new_issue" />
-				</h2>
-				<h2>
-					Draft title:&nbsp;
-					<input type="text" id="new_title" />
-				</h2>
-				<h3>
-					Draft authors:&nbsp;
-					<input type="text" id="new_contributors" />
-				</h3>
-				<div>
-					<input
-						type="file"
-						accept="image/png, image/jpg, image/jpeg"
-						id="upload_cover_image"
-					/>
-					<button onClick={upload_cover_image}>Upload Image</button>
-				</div>
-				{coverImageURL ? (
-					<img id="cover_image" src={coverImageURL} />
-				) : (
-					<></>
-				)}
+  const upload_cover_image = async () => {
+    // In case event listener remains, or is triggered manually, etc
+    const uploaded_files = (
+      document.getElementById("upload_cover_image") as any
+    ).files;
+    if (uploaded_files.length > 0) {
+      const uploaded_file = uploaded_files[0]; // always grab first
+      const public_url = (await upload_image_helper(uploaded_file)) as string;
+      setCoverImageURL(public_url);
+    }
+  };
 
-				<h3>
-					Image by&nbsp;
-					<input type="text" id="new_cover_image_contributor" />
-				</h3>
-				<h3>
-					Summary: &nbsp;
-					<textarea id="new_summary" />
-				</h3>
-				<h3>Section:&nbsp;</h3>
-				<select id="new_section">
-					<option value="0">News</option>
-					<option value="1">Features</option>
-					<option value="2">Opinions</option>
-					<option value="3">Science</option>
-					<option value="4">Humor</option>
-					<option value="5">Sports</option>
-					<option value="6">Arts and Entertainment</option>
-					<option value="7">Media</option>
-					<option value="8">Spec+</option>
-				</select>
-				<h3>The article text:</h3>
-				<textarea id="new_text" />
-				<input onClick={new_draft_handler} type="submit"></input>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Create Draft</h1>
+      <div id="new_draft_fom">
+        <h2>
+          Volume:&nbsp;
+          <input type="number" id="new_volume" />
+          &nbsp;|&nbsp; Issue:&nbsp;
+          <input type="number" id="new_issue" />
+        </h2>
+        <h2>
+          Draft title:&nbsp;
+          <input type="text" id="new_title" />
+        </h2>
+        <h3>
+          Draft authors:&nbsp;
+          <input type="text" id="new_contributors" />
+        </h3>
+        <div>
+          <input
+            type="file"
+            accept="image/png, image/jpg, image/jpeg"
+            id="upload_cover_image"
+          />
+          <button onClick={upload_cover_image}>Upload Image</button>
+        </div>
+        {coverImageURL ? <img id="cover_image" src={coverImageURL} /> : <></>}
+        <h3>
+          Image by&nbsp;
+          <input type="text" id="new_cover_image_contributor" />
+        </h3>
+        <h3>
+          Summary: &nbsp;
+          <textarea id="new_summary" />
+        </h3>
+        <h3>Section:&nbsp;</h3>
+        <select id="new_section">
+          <option value="0">News</option>
+          <option value="1">Features</option>
+          <option value="2">Opinions</option>
+          <option value="3">Science</option>
+          <option value="4">Humor</option>
+          <option value="5">Sports</option>
+          <option value="6">Arts and Entertainment</option>
+          <option value="7">Media</option>
+          <option value="8">Spec+</option>
+        </select>
+        <h3>The article text:</h3>
+        <Editor
+          editorState={editorState}
+          toolbarClassName="toolbarClassName"
+          wrapperClassName="wrapperClassName"
+          editorClassName="editorClassName"
+          onEditorStateChange={setEditorState}
+        />
+        <input onClick={new_draft_handler} type="submit"></input>
+      </div>
+    </div>
+  );
 }
 
 export default Create_Draft;
